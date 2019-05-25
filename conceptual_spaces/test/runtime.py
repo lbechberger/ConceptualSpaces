64a239ff70fadb416a619b673c33e41fedc0f992
Lucas Bechberger <lbechberger@uos.de>
2019-05-25 22:45:32 +0200

    removed deprecated files, updated runtime code

--- conceptual_spaces/test/runtime.py
+++ conceptual_spaces/test/runtime.py
@@ -9,7 +9,7 @@ Created on Thu Jun 29 10:07:24 2017
 import sys
 sys.path.append("..")
 import cs.cs
-from timeit import default_timer as timer
+import time
 import random
 from math import sqrt
 import numpy as np
@@ -34,16 +34,24 @@ def random_weights(domains):
         dim_weights[dom] = local_dim_weights
     return cs.weights.Weights(dom_weights, dim_weights)        
 
-def runtime_intersection(n, num_samples, max_dim_per_domain):
-    """Computes runtime statistics for the intersection operation of concepts.
+def runtime(n_dims, cuboids_per_concept, alpha, method, num_samples, max_dim_per_domain, operation):
+    """Computes runtime statistics for the givenoperation of concepts.
     
-    Parameters: n = number of dimensions, num_samples: number of samples to draw, max_dim_per_domain: maximal number of dimensions per domain."""
-    
-    dimensions = range(n)
-    random.seed(1)
+    Parameters: 
+        n_dims = number of dimensions
+        cuboids_per_concept: number of cuboids per concept
+        alpha = number of alpha cuts to use
+        method = method of computation to use
+        num_samples: number of samples to draw
+        max_dim_per_domain: maximal number of dimensions per domain
+        operation: operation to time"""
+
+    dimensions = range(n_dims)
+    random.seed(42)
     
     times = []
     for i in range(num_samples):
+#        print(i)
         # create a  random domain structure
         domains = {}
         dimensions_left = dimensions
@@ -53,132 +61,72 @@ def runtime_intersection(n, num_samples, max_dim_per_domain):
             dims = random.sample(dimensions_left, num_dims)
             domains[j] = list(dims)
             dimensions_left = [dim for dim in dimensions_left if dim not in dims]
-            j+= 1
-           
+            j += 1
+
         # make the conceptual space
-        cs.cs.init(n, domains)
+        cs.cs.init(n_dims, domains)
         
-        # create two concepts with random identical weights, random cuboids, maximal mu and random c
+        # create three concepts with random identical weights, random cuboids, maximal mu and random c
         w = random_weights(domains)
-        f1 = cs.concept.Concept(cs.core.Core([random_cuboid(dimensions, domains, 0.0, 1.0)], domains), 1.00, random.uniform(1.0, 50.0), w)
-        f2 = cs.concept.Concept(cs.core.Core([random_cuboid(dimensions, domains, 0.0, 1.0)], domains), 1.00, random.uniform(1.0, 50.0), w)
-
-        start = timer()
-        f1.intersect_with(f2)
-        end = timer()
-        duration = end-start
-        times.append(duration)
-    
-    min_time = min(times)
-    max_time = max(times)
-    mean_time = sum(times) / num_samples
-    std_time = sqrt(sum(map(lambda x: (x - mean_time)**2, times))/num_samples)
-
-    print("{0},{1},{2},{3},{4}".format(n, min_time*1000, max_time*1000, mean_time*1000, std_time*1000))
-
-def runtime_size(n, num_samples, max_dim_per_domain):
-    """Computes runtime statistics for the size operation of concepts.
-    
-    Parameters: n = number of dimensions, num_samples: number of samples to draw, max_dim_per_domain: maximal number of dimensions per domain."""
-    dimensions = range(n)
-    random.seed(1)
-    
-    times = []
-    for i in range(num_samples):
-        # create a  random domain structure
-        domains = {}
-        dimensions_left = dimensions
-        j = 0
-        while len(dimensions_left)  > 0:
-            num_dims = random.randint(1, min(len(dimensions_left), max_dim_per_domain))
-            dims = random.sample(dimensions_left, num_dims)
-            domains[j] = list(dims)
-            dimensions_left = [dim for dim in dimensions_left if dim not in dims]
-            j+= 1
-           
-        # make the conceptual space
-        cs.cs.init(n, domains)
+        c1_list = []
+        c2_list = []
+        c3_list = []
+        for i in range(cuboids_per_concept):
+            c1_list.append(random_cuboid(dimensions, domains, 0.0, 1.0))
+            c2_list.append(random_cuboid(dimensions, domains, 0.0, 1.0))
+            c3_list.append(random_cuboid(dimensions, domains, 0.0, 1.0))
+        s1 = cs.core.from_cuboids(c1_list, domains)
+        s2 = cs.core.from_cuboids(c2_list, domains)
+        s3 = cs.core.from_cuboids(c3_list, domains)
         
-        # create a concept with random weights, random cuboids, random mu and random c
-        f = cs.concept.Concept(cs.core.Core([random_cuboid(dimensions, domains, 0.0, 1.0)], domains), random.uniform(0.01, 1.0), random.uniform(1.0, 50.0), random_weights(domains))
+        f1 = cs.concept.Concept(s1, random.uniform(0.01, 1.0), random.uniform(1.0, 50.0), w)
+        f2 = cs.concept.Concept(s2, random.uniform(0.01, 1.0), random.uniform(1.0, 50.0), w)
+        f3 = cs.concept.Concept(s3, random.uniform(0.01, 1.0), random.uniform(1.0, 50.0), w)
 
-        start = timer()
-        f.size()
-        end = timer()
+        start = time.time()
+        operation(f1, f2, f3, n_dims, cuboids_per_concept, method, alpha)
+        end = time.time()
         duration = end-start
         times.append(duration)
-    
     min_time = min(times)
     max_time = max(times)
     mean_time = sum(times) / num_samples
     std_time = sqrt(sum(map(lambda x: (x - mean_time)**2, times))/num_samples)
 
-    print("{0},{1},{2},{3},{4}".format(n, min_time*1000, max_time*1000, mean_time*1000, std_time*1000))
-
-def runtime_size_approx(n, num_samples, max_dim_per_domain):
-    """Computes runtime statistics for numerically approximating the size of a concept.
-    
-    Parameters: n = number of dimensions, num_samples: number of samples to draw, max_dim_per_domain: maximal number of dimensions per domain."""
-    dimensions = range(n)
-    random.seed(1)
-    
-    times = []
-    for i in range(num_samples):
-        # create a  random domain structure
-        domains = {}
-        dimensions_left = dimensions
-        j = 0
-        while len(dimensions_left)  > 0:
-            num_dims = random.randint(1, min(len(dimensions_left), max_dim_per_domain))
-            dims = random.sample(dimensions_left, num_dims)
-            domains[j] = list(dims)
-            dimensions_left = [dim for dim in dimensions_left if dim not in dims]
-            j+= 1
-           
-        # make the conceptual space
-        cs.cs.init(n, domains)
-        
-        # create a concept with random weights, random cuboids, random mu and random c
-        f = cs.concept.Concept(cs.core.Core([random_cuboid(dimensions, domains, 0.0, 1.0)], domains), random.uniform(0.01, 1.0), random.uniform(1.0, 50.0), random_weights(domains))
-        borders = [[-np.inf,np.inf]]*n 
-        def membership(*args):
-            return f.membership_of(args)
-        start = timer()
-        nquad(membership, borders)
-        end = timer()
-        duration = end-start
-        times.append(duration)
-    
-    min_time = min(times)
-    max_time = max(times)
-    mean_time = sum(times) / num_samples
-    std_time = sqrt(sum(map(lambda x: (x - mean_time)**2, times))/num_samples)
-
-    print("{0},{1},{2},{3},{4}".format(n, min_time*1000, max_time*1000, mean_time*1000, std_time*1000))
-    
-  
+    print("{0},{1},{2},{3},{4},{5},{6},{7}".format(n_dims, cuboids_per_concept, alpha, method, min_time*1000, max_time*1000, mean_time*1000, std_time*1000))
+      
 ####################################################################################################################################  
 # MAIN: here we select what to run at all
-run_intersection = False
-run_size = False
-run_size_approx = True
-list_of_n = [1,2,4,8,16,32,64,128]
+config_to_run = 'intersection'
 
-if run_intersection: 
-    print("INTERSECTION")
-    print("n, min_time, max_time, mean_time, std_time")
-    for n in list_of_n:
-        runtime_intersection(n, 10000, 5)
+params = {}
+params['intersection'] = [{'n': [1,2,4,8,16,32,64,128,256,512], 'c': [1], 'a': [20], 'm': ['n/a'], 'r': 1000}, 
+                          {'n': [8], 'c': [1,2,4,8,16], 'a': [20], 'm': ['n/a'], 'r': 1000}]
+params['size'] = [{'n': [1,2,4,8,16,32], 'c': [1], 'a': [20], 'm': ['n/a'], 'r': 1000}, 
+                  {'n': [4], 'c': [1,2,4,8,16], 'a': [20], 'm': ['n/a'], 'r': 1000}]
+params['size_approx'] = [{'n': [1,2,4], 'c': [1], 'a': [20], 'm': ['n/a'], 'r': 100}, 
+                         {'n': [2], 'c': [1,2,4], 'a': [20], 'm': ['n/a'], 'r': 100}]
+params['betweenness'] = [{'n': [1,2,4,8,16], 'c': [1], 'a': [20], 'm': ['minimum', 'integral'], 'r': 1000}, 
+                         {'n': [8], 'c': [1,2,4,8], 'a': [20], 'm': ['minimum', 'integral'], 'r': 1000},
+                         {'n': [8], 'c': [1], 'a': [20,50,100,500], 'm': ['minimum', 'integral'], 'r': 1000}]
 
-if run_size: 
-    print("SIZE")
-    print("n, min_time, max_time, mean_time, std_time")
-    for n in list_of_n:
-        runtime_size(n, 10000, 5)
+# helper function for the numerical approximation of the size integral
+def approx_helper(x, y, z, n, c, m, a):
+    borders = [[-np.inf,np.inf]]*n 
+    def membership(*args):
+        return x.membership_of(args)
+    nquad(membership, borders)   
 
+operations = {'intersection': lambda x,y,z,n,c,m,a: x.intersect_with(y),
+              'size': lambda x,y,z,n,c,m,a: x.size(),
+              'size_approx': lambda x,y,z,n,c,m,a: approx_helper(x,y,z,n,c,m,a),
+              'betweenness': lambda x,y,z,n,c,m,a: x.between(y,z, method=m, num_alpha_cuts=a)}
 
-if run_size_approx: 
-    print("SIZE APPROX")
-    print("n, min_time, max_time, mean_time, std_time")
-    for n in list_of_n:
-        runtime_size_approx(n, 10000, 5)
+for param_set in params[config_to_run]:
+    print("\n{0} - {1} repetitions".format(config_to_run, param_set['r']))
+    print("n,c,alpha,method,min_time,max_time,mean_time,std_time")
+    for n in param_set['n']:
+        for c in param_set['c']:
+            for a in param_set['a']:
+                for m in param_set['m']:
+                    runtime(n, c, a, m, param_set['r'], 5, operations[config_to_run])
